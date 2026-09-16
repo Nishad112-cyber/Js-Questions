c3a14f83d519060395e54c65d4710a63243e0a05
Nishad112-cyber <nishadguptad@gmail.com>
2026-09-16 09:51:25 +0530

    modification

--- Coding/GetSmallest.js
+++ Coding/GetSmallest.js
@@ -1,6 +1,6 @@
 function getSmallest(arr){
     let smallest =arr[0];
-    for(let i=0; i<=arr.length; i++){
+    for(let i=1; i<arr.length; i++){
         if(arr[i]<smallest){
               smallest  = arr[i];
         }
