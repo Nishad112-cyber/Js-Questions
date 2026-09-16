672710007a449cb7fb1ccd87e8d3f665f68c4d17
Nishad112-cyber <nishadguptad@gmail.com>
2026-09-16 10:14:14 +0530

    add new code

--- Coding/ReverseString2.js
+++ Coding/ReverseString2.js
@@ -1,5 +1,8 @@
-function reverseString(str){
-    return str.split("").reverse().join("");
+let str= "hello";
+let reverse= "";
+for(let i= str.length-1; i>=0; i--){
+    reverse += str[i];
 
 }
-console.log(reverseString("Hello"));
+
+console.log(reverse);
